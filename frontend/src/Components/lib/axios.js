18d12128d5b71ba7c7c63a015e513c878b1115dd
Yu Waddy <yuwaddylwin.2004@gmail.com>
2026-05-20 16:50:22 +0700

    fix production api url

--- frontend/src/Components/lib/axios.js
+++ frontend/src/Components/lib/axios.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
+const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://unimarket-08di.onrender.com/api"
+    : "http://localhost:8000/api";
+
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000/api",
-    withCredentials: true,
-})
+  baseURL: BASE_URL,
+  withCredentials: true,
+});
